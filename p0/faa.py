fb777f4c9582708fff17767f4bdb5dc6d14ed234
Sergio Sanz <sanz.cebollero@zoho.com>
2016-09-26 07:02:03 +0200

    Cambios menores

--- p0/faa.py
+++ p0/faa.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+ def leer_dato(fichero):
+  """Lee una linea del fichero y divide la linea en tantos trozos como comas"""
+  return fichero.readline().split('\n')[0].split(',')
+
 class Datos(object):
   
   supervisado=True
@@ -13,6 +17,16 @@ class Datos(object):
  
   # TODO: procesar el fichero para asignar correctamente las variables supervisado, tipoAtributos, nombreAtributos,nominalAtributos, datos y diccionarios
   def __init__(self, nombreFichero,sup):
+    self.supervisado = sup
+    dataFile = open(nombreFichero,'r')
+    numDatos = int(dataFile.readline())
+    nombreAtributos = leer_dato(dataFile)
+    tipoAtributos = leer_dato(dataFile)
+    datos = np.array([leer_dato(dataFile) for x in xrange(numDatos)])
+    
+    
+
+    
     
   # TODO: hacer en las proximas practicas
   def extraeDatosTrain(idx):
