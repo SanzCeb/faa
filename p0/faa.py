1cefe4dc8c156a776c1221abdfdab1048f3cdfd4
Sergio Sanz <sanz.cebollero@zoho.com>
2016-09-26 17:30:53 +0200

    Version 1.0 de la practica 0

--- p0/faa.py
+++ p0/faa.py
@@ -1,8 +1,53 @@
 import numpy as np
 
- def leer_dato(fichero):
+def leer_dato(fichero):
   """Lee una linea del fichero y divide la linea en tantos trozos como comas"""
   return fichero.readline().split('\n')[0].split(',')
+
+def leer_metadatos(fichero):
+  """Funcion que lee el fichero y devuelve el numero de datos, 
+  la cabecera de los datos y el tipo de cada atributo"""
+  return int(fichero.readline()),leer_dato(fichero),leer_dato(fichero)
+
+def leer_datos(fichero,numDatos):
+  """Funcion para leer los numDatos disponibles"""
+  return [leer_dato(fichero) for x in range(numDatos)]
+
+def leer_fichero(fichero):
+  """Funcion para leer los ficheros .data"""
+  numDatos, atributos, tipoAtributos = leer_metadatos(fichero)
+  datos = leer_datos(fichero,numDatos)
+  return atributos,tipoAtributos,datos
+
+def es_nominal(tipo):
+  return True if tipo == 'Nominal' else False
+
+def encontrar_nominales(tipoAtributos):
+  """Funcion para averiguar que atributos son nominales"""
+  return list(map(es_nominal, tipoAtributos)) 
+
+def crear_diccionario(seq):
+  """Funcion que asigna un codigo a cada valor unico de seq 
+  y devuelve un diccionario"""
+  conjunto = list(set(seq))
+  return dict(zip(conjunto,range(len(conjunto))))
+
+def crear_diccionarios(datos):
+  """Funcion que crea un diccionario por cada atributo 
+  que posea el conjunto de datos"""
+  datosTraspuestos = np.transpose(np.array(datos))
+  return list(map(crear_diccionario, datosTraspuestos))
+
+def codificar_atributo(diccionario, valores):
+  """Funcion que transforma cada valor en su clave correspondiente en el 
+  diccionario"""
+  return [diccionario[i] for i in valores]
+
+
+def codificar_datos(diccionarios, matriz_datos):
+  """Funcion que codifica el conjunto de datos leidos del fichero"""
+  return list(map(codificar_atributo,diccionarios,np.transpose(matriz_datos)))
+
 
 class Datos(object):
   
@@ -12,22 +57,17 @@ class Datos(object):
   nombreAtributos=[]
   nominalAtributos=[]
   datos=np.array(())
-  # Lista de diccionarios. Uno por cada atributo.
   diccionarios=[]
  
-  # TODO: procesar el fichero para asignar correctamente las variables supervisado, tipoAtributos, nombreAtributos,nominalAtributos, datos y diccionarios
+
   def __init__(self, nombreFichero,sup):
     self.supervisado = sup
-    dataFile = open(nombreFichero,'r')
-    numDatos = int(dataFile.readline())
-    self.nombreAtributos = leer_dato(dataFile)
-    self.tipoAtributos = leer_dato(dataFile)
-    self.nominalAtributos = map(lambda x: True if x == 'Nominal' else False,tipoAtributos)
-    self.datos = np.array([leer_dato(dataFile) for x in xrange(numDatos)])
-    dataFile.close()
-    
-
-    
+    fichero = open(nombreFichero,'r')
+    self.nombreAtributos, self.tipoAtributos, datos_raw = leer_fichero(fichero)
+    self.nominalAtributos = encontrar_nominales(self.tipoAtributos)
+    self.diccionarios = crear_diccionarios(datos_raw)
+    self.datos = np.array(codificar_datos(self.diccionarios,datos_raw))
+    fichero.close()
     
   # TODO: hacer en las proximas practicas
   def extraeDatosTrain(idx):
@@ -37,5 +77,7 @@ class Datos(object):
     pass
 
 
+dataset = Datos('ConjuntosDatos/tic-tac-toe.data',True)
 
+print(dataset.datos)
   
