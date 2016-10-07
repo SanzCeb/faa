0f92689237f6fe93a3c67b7cb5c0f33c3a3fc2b0
Sergio Sanz <sanz.cebollero@zoho.com>
2016-10-07 20:35:01 +0200

    Implementado NaiveBayes para atributos continuos

--- p1/ClasificadorNaiveBayes.py
+++ p1/ClasificadorNaiveBayes.py
@@ -2,68 +2,67 @@ from Clasificador import Clasificador
 import functools as fp
 import operator as op
 import numpy as np
-
+import scipy.stats as stats
+import math
 
 class ClasificadorNaiveBayes(Clasificador):
 
-    entrenamiento = []
-    a_prioris = []
+    tabla_frecuencia = []
+    probs_a_priori = []
 
     def __tabla_discreta (self,
-                          valores,
+                          columna_atributo,
                           hipotesis,
-                          diccionario_atributo,
-                          diccionario_hipotesis):
-        tabla_entrenamiento = list(zip(valores,hipotesis))
+                          conjunto_atributo,
+                          conjunto_hipotesis):
+        """Funcion que realiza el cruce del conjunto de valores de un atributo
+        con el conjunto de hipotesis."""
+        tabla_entrenamiento = list(zip(columna_atributo,hipotesis))
         def __num_coincidencias ( atributo, hipotesis):
             return tabla_entrenamiento.count((atributo,
                                               hipotesis))
 
-        for k_atributo in diccionario_atributo:
-            dicc = { k_hipotesis : __num_coincidencias (k_atributo, k_hipotesis)
-                     for k_hipotesis in diccionario_hipotesis}
-            yield k_atributo,dicc
+        for k_atributo in conjunto_atributo:
+            frecuencias = { k_hipotesis :
+                            __num_coincidencias (k_atributo, k_hipotesis)
+                            for k_hipotesis in conjunto_hipotesis}
+            yield k_atributo,frecuencias
 
-    def __tabla_continua(self,valores, diccionario):
-        pass
 
-    def entrenamiento(self,datos_train,atributosDiscretos,diccionario):
-        datos_traspuestos = datos_train.transpose()
-        datos_atributos = datos_traspuestos[:-1]
-        datos_clase = list(datos_traspuestos[-1])
-        def genera_tabla (valores, datoDiscreto,diccionario_atributo):
-            if datoDiscreto:
-                return dict(self.__tabla_discreta(valores,
-                                                  datos_clase,
-                                                  diccionario_atributo.values(),
-                                                  diccionario[-1].values()))
-            else:
-                return self.__tabla_continua(valores,diccionario)
-            pass
+    def __tabla_continua(self,
+                         columna_atributo,
+                         columna_clase,
+                         conjunto_hipotesis):
+        """Funcion que calcula la gaussiana de un atributo continuo
+        a partir de los datos de entrenamiento"""
+        for k_hipotesis in diccionario_hipotesis:
+            datos_clase = [ valor
+                            for valor in zip (columna_atributo,columna_clase)
+                            if hipotesis == k_hipotesis ]
+            yield k_hipotesis, stats.norm(np.mean(datos_clase),
+                                          np.std(datos_clase))
 
-        num_datos_train = len(datos_clase)
-        self.a_prioris = [ datos_clase.count(dicc[key]) / num_datos_train
-                           for dicc in diccionario
-                           for key in dicc][:-1]
-        self.entrenamiento = list(map(genera_tabla,
-                                      datos_atributos,
-                                      atributosDiscretos,
-                                      diccionario))
+
 
 
     def __calcular_verosimilitud_discreta(self,
                                           frecuencias,
                                           atributo,
                                           hipotesis ):
+        """Calcula la verosimilitud de la hipotesis a partir
+        del valor del atributo del dato que queremos clasificar
+        y de las frecuencias recogidas durante el entrenamiento"""
         casos_favorables = frecuencias[atributo][hipotesis]
         casos_posibles = sum(frecuencias[atributo].values())
-        return casos_favorables / casos_posibles
+        #Correccion  con logaritmos para evitar valores muy cerca de cero
+        return math.log(casos_favorables / casos_posibles)
     # Implementar formula gaussiana
     def __calcular_verosimilitud_continua(self,
-                                          frecuencias,
                                           atributo,
+                                          frecuencias,
                                           hipotesis):
-        return 0
+        return frecuencias[hipotesis].pdf(atributo)
+
 
     def  __calcula__verosimilitud(self,
                                   frecuencias,
@@ -76,26 +75,56 @@ class ClasificadorNaiveBayes(Clasificador):
                                                            hipotesis)
         else:
             return self.__calcular_verosimilitud_continua (frecuencias,
-                                                           atributo,
                                                            hipotesis)
+
 
     def __clasifica_dato (self,dato, atributosDiscretos, diccionario ):
         def __num_casos_a_posteriori ( hipotesis ):
-            verosimilitudes = map(lambda x,y,z:
-                                  self.__calcula__verosimilitud (x,y,z,
+            verosimilitudes = map(lambda frecuencias,atributo,esDiscreto:
+                                  self.__calcula__verosimilitud (frecuencias,
+                                                                 atributo,
+                                                                 esDiscreto,
                                                                  hipotesis),
-                                  self.entrenamiento,
+                                  self.tabla_frecuencia,
                                   dato,
                                   atributosDiscretos)
-            return fp.reduce(op.mul,verosimilitudes) * self.a_prioris[hipotesis]
+            prob_a_priori = self.probs_a_priori[hipotesis]
+            return fp.reduce(op.mul,verosimilitudes) * prob_a_priori
+
         casos_favorables  = list(map( __num_casos_a_posteriori,
                                       diccionario[-1].values()))
+
         return casos_favorables.index(max(casos_favorables))
+
+
+    def entrenamiento(self,datos_train,atributosDiscretos,diccionario):
+        datos_traspuestos = datos_train.transpose()
+        datos_atributos = datos_traspuestos[:-1]
+        datos_clase = list(datos_traspuestos[-1])
+
+        def __genera_tabla (valores, datoDiscreto,diccionario_atributo):
+            if datoDiscreto:
+                return dict(self.__tabla_discreta(valores,
+                                                  datos_clase,
+                                                  diccionario_atributo.values(),
+                                                  diccionario[-1].values()))
+            else:
+                return self.__tabla_continua(valores,
+                                             datos_clase,
+                                             diccionario[-1].values())
+
+        num_datos_train = len(datos_clase)
+        self.probs_a_priori = [ datos_clase.count(dicc[key]) / num_datos_train
+                                for dicc in diccionario
+                                for key in dicc][:-1]
+
+        self.tabla_frecuencia = list(map(__genera_tabla,
+                                      datos_atributos,
+                                      atributosDiscretos,
+                                      diccionario))
+
     def clasifica(self,datostest,atributosDiscretos,diccionario):
         return [ self.__clasifica_dato (dato,atributosDiscretos,diccionario)
                  for dato in datostest ]
 
-#        casos_favorables = list(map(__calcular_casos_favorables,
- #                                   diccionario[-1]))
-  #      clase_mayoritaria = diccionario[casos_favorables.
-   #                                          index(max(casos_favorables))]
+
