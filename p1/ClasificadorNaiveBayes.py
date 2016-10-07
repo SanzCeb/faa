2436e87e1daf9d82964066ef091308fda8ba6145
Sergio Sanz <sanz.cebollero@zoho.com>
2016-10-07 19:06:09 +0200

    El clasificador Naive-Bayes discreto parece que funciona, falta documentar y e implementar el soporte a valores continuos

--- p1/ClasificadorNaiveBayes.py
+++ p1/ClasificadorNaiveBayes.py
@@ -1,89 +1,101 @@
 from Clasificador import Clasificador
+import functools as fp
+import operator as op
 import numpy as np
+
 
 class ClasificadorNaiveBayes(Clasificador):
 
     entrenamiento = []
     a_prioris = []
 
-
-    def __tabla_discreta (valores,hipotesis,
+    def __tabla_discreta (self,
+                          valores,
+                          hipotesis,
                           diccionario_atributo,
                           diccionario_hipotesis):
-
         tabla_entrenamiento = list(zip(valores,hipotesis))
         def __num_coincidencias ( atributo, hipotesis):
-            return tabla_entrenamiento.count((atributo,hipotesis)) 
+            return tabla_entrenamiento.count((atributo,
+                                              hipotesis))
 
         for k_atributo in diccionario_atributo:
-                yield k_atributo,
-                {
-                    k_hipotesis : __num_coincidencias (k_atributo, k_hipotesis)
-                    for k_hipotesis in diccionario_hipotesis
-                }
+            dicc = { k_hipotesis : __num_coincidencias (k_atributo, k_hipotesis)
+                     for k_hipotesis in diccionario_hipotesis}
+            yield k_atributo,dicc
 
-    def __tabla_continua(valores, diccionario):
+    def __tabla_continua(self,valores, diccionario):
         pass
 
     def entrenamiento(self,datos_train,atributosDiscretos,diccionario):
-        datos_clase = list(datos_train.transpose()[-1])
-        def __genera_tabla (valores, datoDiscreto,diccionario_atributo):
+        datos_traspuestos = datos_train.transpose()
+        datos_atributos = datos_traspuestos[:-1]
+        datos_clase = list(datos_traspuestos[-1])
+        def genera_tabla (valores, datoDiscreto,diccionario_atributo):
             if datoDiscreto:
-                return __tabla_discreta(valores,
-                                        datos_clase,
-                                        diccionario_atributo.values(),
-                                        diccionario[-1].values())
+                return dict(self.__tabla_discreta(valores,
+                                                  datos_clase,
+                                                  diccionario_atributo.values(),
+                                                  diccionario[-1].values()))
             else:
-                return __tabla_continua(valores,diccionario)
+                return self.__tabla_continua(valores,diccionario)
             pass
 
         num_datos_train = len(datos_clase)
         self.a_prioris = [ datos_clase.count(dicc[key]) / num_datos_train
                            for dicc in diccionario
-                           for key in dicc][:-1] ]
-        self.entrenamiento = list(map(__genera_tabla,
-                                      datos_clase,
+                           for key in dicc][:-1]
+        self.entrenamiento = list(map(genera_tabla,
+                                      datos_atributos,
                                       atributosDiscretos,
                                       diccionario))
 
 
-    def __calcular_verosimilitud_discreta( frecuencias,
-                                           atributo,
-                                           hipotesis ):
+    def __calcular_verosimilitud_discreta(self,
+                                          frecuencias,
+                                          atributo,
+                                          hipotesis ):
         casos_favorables = frecuencias[atributo][hipotesis]
-        casos_posibles = sum(frecuencias[valor_atributo].values())
+        casos_posibles = sum(frecuencias[atributo].values())
         return casos_favorables / casos_posibles
     # Implementar formula gaussiana
-    def __calcular_verosimilitud_continua:
+    def __calcular_verosimilitud_continua(self,
+                                          frecuencias,
+                                          atributo,
+                                          hipotesis):
         return 0
 
+    def  __calcula__verosimilitud(self,
+                                  frecuencias,
+                                  atributo,
+                                  esDiscreto,
+                                  hipotesis):
+        if esDiscreto:
+            return  self.__calcular_verosimilitud_discreta(frecuencias,
+                                                           atributo,
+                                                           hipotesis)
+        else:
+            return self.__calcular_verosimilitud_continua (frecuencias,
+                                                           atributo,
+                                                           hipotesis)
 
-    def __clasifica_dato ( dato, atributosDiscretos, diccionario ):
+    def __clasifica_dato (self,dato, atributosDiscretos, diccionario ):
         def __num_casos_a_posteriori ( hipotesis ):
-            def  __calcula__verosimilitud(frecuencias,atributo,esDiscreto):
-                if esDiscreto:
-                   return  __calcular_verosimilitud_discreta(frecuencias,
-                                                             atributo,
-                                                             hipotesis)
-                else:
-                    return __calcular_verosimilitud_continua (frecuencias,
-                                                              atributo,
-                                                              hipotesis)
-            verosimilitudes = map(__calcula_verosimilitud,
-                                  entrenamiento,
+            verosimilitudes = map(lambda x,y,z:
+                                  self.__calcula__verosimilitud (x,y,z,
+                                                                 hipotesis),
+                                  self.entrenamiento,
                                   dato,
                                   atributosDiscretos)
-            return reduce (lambda x,y: x * y ,
-                           verosimilitudes) * a_priori[hipotesis]
+            return fp.reduce(op.mul,verosimilitudes) * self.a_prioris[hipotesis]
         casos_favorables  = list(map( __num_casos_a_posteriori,
                                       diccionario[-1].values()))
         return casos_favorables.index(max(casos_favorables))
     def clasifica(self,datostest,atributosDiscretos,diccionario):
-        return [ clasifica_dato (dato,atributosDiscretos,diccionario)
+        return [ self.__clasifica_dato (dato,atributosDiscretos,diccionario)
                  for dato in datostest ]
 
 #        casos_favorables = list(map(__calcular_casos_favorables,
  #                                   diccionario[-1]))
   #      clase_mayoritaria = diccionario[casos_favorables.
    #                                          index(max(casos_favorables))]
-                                                
