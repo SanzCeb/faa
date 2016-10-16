c9ddc87ef2de887a94232c0348d88115862ef869
Sergio Sanz <sanz.cebollero@zoho.com>
2016-10-16 13:37:04 +0200

    Modificado test para la version entregable

--- p1/practica1_test.py
+++ p1/practica1_test.py
@@ -6,25 +6,26 @@ from ClasificadorNaiveBayesLaplace import ClasificadorNaiveBayesLaplace
 import EstrategiaParticionado
 import numpy
 
-
-nombre_script,nombre_fichero = argv
-
-
-dataset = Datos ( nombre_fichero, True)
 estrategia = EstrategiaParticionado.ValidacionCruzada(5)
 clasificador = ClasificadorAPriori()
 clasificador_bayes = ClasificadorNaiveBayes()
 clasificador_laplace = ClasificadorNaiveBayesLaplace()
 
+def clasificar (dataset):
+    errores_bayes  = clasificador.validacion(estrategia,dataset,clasificador_bayes)
+    errores_laplace = clasificador.validacion(estrategia,dataset,clasificador_laplace)
+    print ("Error del clasificador Naive-Bayes es: ", numpy.mean(errores_bayes),numpy.std(errores_bayes))
+    print ("Error del clasificador Naive-Bayes-Laplace es: ", numpy.mean(errores_laplace),numpy.std(errores_laplace))
+    print ()
 
+    
 
+tic_tac_toe = Datos ("../ficheros/ConjuntosDatos/tic-tac-toe.data", True)
+wine = Datos ( "../ficheros/ConjuntosDatos/wine_proc.data", True)
+crx = Datos ( "../ficheros/ConjuntosDatos/crx.data", True)
+digits = Datos ( "../ficheros/ConjuntosDatos/digits.data",True)
 
-errores = clasificador.validacion(estrategia, dataset, clasificador)
-errores_bayes  = clasificador.validacion(estrategia,dataset,clasificador_bayes)
-errores_laplace = clasificador.validacion(estrategia,dataset,clasificador_laplace)
-
-
-print ("Error del clasificador a priori es: ", numpy.mean(errores))
-print ("Error del clasificador Naive-Bayes es: ", numpy.mean(errores_bayes))
-print ("Error del clasificador Naive-Bayes-Laplace es: ", numpy.mean(errores_laplace))
-
+clasificar(tic_tac_toe)
+clasificar(wine)
+clasificar(crx)
+clasificar(digits)
