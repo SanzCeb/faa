814266f8c45a05a5144d95c01aeea27df5159544
sigblock <sergio13fuenla@gmail.com>
2016-11-03 19:44:15 +0100

    Correcciones en el algoritmo de regresion logistica. Incluidos dos metodos en la clase clasificador y adjuntada la implementacion de referencia del clasificador multiclase

--- p2/Clasificador.py
+++ p2/Clasificador.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta,abstractmethod
-
+import numpy as np 
 
 class Clasificador(object):
   
@@ -17,12 +17,18 @@ class Clasificador(object):
     pass
   
   
-  @abstractmethod
+  
   # TODO: esta funcion deben ser implementadas en cada clasificador concreto
   # devuelve un numpy array con las predicciones
   def clasifica(self,datosTest,atributosDiscretos,diccionario):
-    pass
-  
+    scores = self.score(datosTest,atributosDiscretos, diccionario)
+	return np.argmax(scores, axis = 1)
+  def score (self, datosTest, atributosDiscretos, diccionario):
+	scores = np.zeros((len(datosTest), len(diccionario[-1])))
+	preds = map(int, self.clasifica(datosTest,atributosDiscretos, diccionario))
+	scores[range(datosTest.shape[0]), preds] = 1.0 
+	return scores 
+	
   
   def __error(self,datos,pred):
     """Se cuenta tanto el numero de errores como el total de 
