823f435cb8b07f3e2bdce04b2fc349c77f843e1b
sigblock <sergio13fuenla@gmail.com>
2016-11-08 20:52:44 +0100

    Corregidos algunos errores del clasificador naive bayes, actualizado el script de pruebas del apartado uno de la memoria y actualizado el fichero crx con la nueva version disponible 'crx_clean'

---
+++ p2/ClasificadorNaiveBayes.py
@@ -0,0 +1,137 @@
+from Clasificador import Clasificador
+import functools as fp
+import operator as op
+import numpy as np
+import scipy.stats as stats
+import math
+
+class ClasificadorNaiveBayes(Clasificador):
+
+    tabla_frecuencia = []
+    probs_a_priori = []
+
+    def __tabla_discreta (self,
+                          columna_atributo,
+                          hipotesis,
+                          conjunto_atributo,
+                          conjunto_hipotesis):
+        """Funcion que realiza el cruce del conjunto de valores de un atributo
+        con el conjunto de hipotesis."""
+        tabla_entrenamiento = list(zip(columna_atributo,hipotesis))
+        def __num_coincidencias ( atributo, hipotesis):
+            return tabla_entrenamiento.count((atributo,
+                                              hipotesis))
+
+        for k_atributo in conjunto_atributo:
+            frecuencias = { k_hipotesis :
+                            __num_coincidencias (k_atributo, k_hipotesis)
+                            for k_hipotesis in conjunto_hipotesis}
+            yield k_atributo,frecuencias
+
+
+    def __tabla_continua(self,
+                         columna_atributo,
+                         columna_clase,
+                         conjunto_hipotesis):
+        """Funcion que genera la funcion de un atributo continuo
+        a partir de los datos de entrenamiento"""
+        inc = pow(10,-6)
+        for k_hipotesis in conjunto_hipotesis:
+            datos_clase = [ valor
+                            for (valor,hipotesis) in
+                            zip (columna_atributo,columna_clase)
+                            if hipotesis == k_hipotesis ]
+            gaussiana = stats.norm(np.mean(datos_clase),
+                                   np.std(datos_clase) + inc)
+            yield k_hipotesis, gaussiana.pdf
+
+
+
+
+    def __calcular_verosimilitud_discreta(self,
+                                          frecuencias,
+                                          atributo,
+                                          hipotesis ):
+        """Calcula la verosimilitud de la hipotesis a partir
+        del valor del atributo del dato que queremos clasificar
+        y de las frecuencias recogidas durante el entrenamiento"""
+        casos_favorables = frecuencias[atributo][hipotesis]
+        casos_posibles = sum(frecuencias[atributo].values())
+        
+        return 0 if casos_posibles == 0 else (casos_favorables / casos_posibles)
+
+    def __calcular_verosimilitud_continua(self,
+                                          frecuencias,
+                                          atributo,
+                                          hipotesis):
+        return frecuencias[hipotesis](atributo)
+
+
+    def  __calcula__verosimilitud(self,
+                                  frecuencias,
+                                  atributo,
+                                  esDiscreto,
+                                  hipotesis):
+        """Funcion que comprueba si el atributo es discreto o no
+        para decidir que funcion debe de calcular la verosimilitud"""
+        if esDiscreto:
+            return self.__calcular_verosimilitud_discreta(frecuencias,
+                                                               atributo,
+                                                               hipotesis)
+        else:
+            return self.__calcular_verosimilitud_continua (frecuencias,
+                                                                atributo,
+                                                                hipotesis)
+
+
+    def __clasifica_dato (self,dato, atributosDiscretos, diccionario ):
+        def __num_casos_a_posteriori ( hipotesis ):
+            verosimilitudes = map(lambda frecuencias,atributo,esDiscreto:
+                                  self.__calcula__verosimilitud (frecuencias,
+                                                                 atributo,
+                                                                 esDiscreto,
+                                                                 hipotesis),
+                                  self.tablas_frecuencias,
+                                  dato[:-1],
+                                  atributosDiscretos[:-1])
+            prob_a_priori = self.probs_a_priori[hipotesis]
+            
+            if prob_a_priori == 0 or 0 in verosimilitudes: 
+                return 0
+            else:
+                return math.log(fp.reduce(op.mul,verosimilitudes,prob_a_priori))
+                
+        conjunto_hipotesis = list(diccionario[-1].values())
+        casos_favorables  = list(map( __num_casos_a_posteriori,conjunto_hipotesis))
+
+        return conjunto_hipotesis[casos_favorables.index(max(casos_favorables))]
+
+
+    def entrenamiento(self,datos_train,atributosDiscretos,diccionario):
+        datos_traspuestos = datos_train.transpose()
+        datos_atributos = datos_traspuestos[:-1]
+        datos_clase = list(datos_traspuestos[-1])
+
+        def __genera_tabla (valores, datoDiscreto,diccionario_atributo):
+            if datoDiscreto:
+                return dict(self.__tabla_discreta(valores,
+                                                  datos_clase,
+                                                  diccionario_atributo.values(),
+                                                  diccionario[-1].values()))
+            else:
+                return dict(self.__tabla_continua(valores,
+                                             datos_clase,
+                                             diccionario[-1].values()))
+
+        num_datos_train = len(datos_clase)
+        self.probs_a_priori = [ datos_clase.count(diccionario[-1][key]) / num_datos_train
+                                for key in diccionario[-1]]
+        self.tablas_frecuencias = list(map(__genera_tabla,
+                                      datos_atributos,
+                                      atributosDiscretos,
+                                      diccionario))
+    def clasifica(self,datostest,atributosDiscretos,diccionario):
+        return [ self.__clasifica_dato (dato,atributosDiscretos,diccionario)
+                 for dato in datostest ]
+
+
--- p2/apartado1.py
+++ p2/apartado1.py
@@ -1,29 +1,42 @@
 import numpy as np 
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
+from ClasificadorNaiveBayes import ClasificadorNaiveBayes
+from ClasificadorNaiveBayesLaplace import ClasificadorNaiveBayesLaplace
 from datos import Datos
-import EstrategiaParticionado
+from EstrategiaParticionado import ValidacionCruzada
 
-def clasifica_sklearn(datos,clasificador):
-    X_train, X_test, y_train, y_test = train_test_split(datos[:,:-1],
-                                                        datos[:,-1],
+clasificador_NB = ClasificadorNaiveBayes()
+clasificador_laplace = ClasificadorNaiveBayesLaplace()
+clasificador_sk = GaussianNB()
+
+def clasificar(datos):
+    X_train, X_test, y_train, y_test = train_test_split(datos.datos[:,:-1],
+                                                        datos.datos[:,-1],
                                                         test_size = 0.2,
                                                         random_state = 42)
-    clasificador.fit(X_train,y_train)
-    error = 1 - clasificador.score(X_test,y_test)
-    print( "Tasa de error: ", error)
+    clasificador_sk.fit(X_train,y_train)
+    print( "Tasa de aciertos sklearn: ", clasificador_sk.score(X_test,y_test) )
+    
+    particionado = ValidacionCruzada(10)                       
+    error = clasificador_NB.validacion(particionado, datos, clasificador_NB)
+    print( "Tasa de aciertos Naive-Bayes: ", 1 - np.mean(error))
+    
+    particionado = ValidacionCruzada(10)                       
+    error = clasificador_NB.validacion(particionado, datos, clasificador_NB)
+    print( "Tasa de aciertos Laplace: ", 1 - np.mean(error))
     return error
 
 
 
-clasificadorNB = GaussianNB()
-tic_tac_toe = Datos ("../ficheros/ConjuntosDatos/tic-tac-toe.data", True).datos
-wine = Datos("../ficheros/ConjuntosDatos/wine_proc.data",True).datos
-crx = Datos("../ficheros/ConjuntosDatos/crx.data",True).datos
-digits = Datos("../ficheros/ConjuntosDatos/digits.data",True).datos
 
-clasifica_sklearn(tic_tac_toe,clasificadorNB)
-clasifica_sklearn(wine,clasificadorNB)
-clasifica_sklearn(crx,clasificadorNB)
-clasifica_sklearn(digits,clasificadorNB)
+tic_tac_toe = Datos ("../ficheros/ConjuntosDatos/tic-tac-toe.data", True)
+wine = Datos("../ficheros/ConjuntosDatos/wine_proc.data",True)
+crx = Datos("../ficheros/ConjuntosDatos/crx.data",True)
+digits = Datos("../ficheros/ConjuntosDatos/digits.data",True)
+
+clasificar(tic_tac_toe)
+clasificar(wine)
+clasificar(crx)
+clasificar(digits)
 
