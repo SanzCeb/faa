a2d2f67a1b9239103e0261eb84905ef8a43aa6dd
Sergio Sanz <sanz.cebollero@zoho.com>
2016-11-25 21:32:04 +0100

    Creado el modulo utils.py para albergar funciones utils para implementar los distintos metodos

---
+++ p3/utils.py
@@ -0,0 +1,39 @@
+import numpy as np
+import random
+from unittest import TestCase
+
+
+def generar_bits (num):
+    """Genera un numpy array con bits aleatorios"""
+    return np.random.rand(num).round()
+
+def entero_aleatorio(a,b):
+    """Devuelve un numero entero aleatorio dentro del 
+    intervalo [a,b]"""
+    random.seed(datetime.now())
+    return random.randint(a,b)
+
+def numero_aleatorio(a,b):
+    """Devuelve un numero flotante aleatorio dentro del 
+    intervalo [a,b]"""
+    random.seed(datetime.now())
+    return random.uniform(a,b)
+
+def crear_esquema(diccionarios):
+    """metodo que devuelve la longitud de cada alelo del cromosoma
+    a partir de la lista de diccionarios"""
+    esquema = []
+    for dicc in diccionarios:
+        num_alelos = len(dicc.keys())
+        if num_alelos:
+            esquema.append(num_alelos)
+    return esquema
+
+class UtilsTests (TestCase):
+        def crear_esquema_test(self):
+            diccionarios = [{'a':0,'b':3,'c':1},{'z':2, 'w':5, 'r': 10,'o':8}, {'x': 0, 'y': '1'}]
+            self.assertEqual(crear_esquema(diccionarios), [3,4,2])
+            diccionarios = [{}]
+            self.assertEqual(crear_esquema(diccionarios), [])
+            diccionarios = [{'hola':1, 'adios':0},{'haciendo':2,'un test': 2}]
+            self.assertEqual(crear_esquema(diccionarios), [2,2])
