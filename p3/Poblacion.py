fe1c9e347f49f32a1d19e562e9c35b3c8df88105
SanzCeb <sanz.cebollero@zoho.com>
2016-11-30 17:53:20 +0100

    La funcion generar_poblacion ahora genera listas en lugar de iteradores. Corregida la implementacion de la funcion escoger Individuo

--- p3/Poblacion.py
+++ p3/Poblacion.py
@@ -5,7 +5,7 @@ from utils import entero_aleatorio
 import math 
 
 class Poblacion:
-    individuos = []
+    individuos = None
     vastagos = []
     poblacion = 0
     num_generaciones = 0.0
@@ -22,9 +22,32 @@ class Poblacion:
                             for i in range(poblacion) ]
         
 
-    def __ordenar_poblacion(self):
-        poblacion = map(lambda x : (x,x.fitness()), individuos)
-        return sorted(poblacion, key = lambda x : x[1])
+    def generar_poblacion(self, tam_poblacion):
+        return [ Individuo(self.esquema, entero_aleatorio(1,5))
+                 for i in range(tam_poblacion)]
+            
+
+    def calcular_num_cruces(self):
+        return math.floor(self.tam_poblacion * self.prob_cruce)
+
+    def calcular_num_mutaciones(self):
+        return round(len(self.vastagos) * self.prob_mutacion)
+
+    def calcular_num_supervivientes(self):
+        return round(self.tam_poblacion * self.prob_elitismo)
+
+    def ordenar_poblacion(self, datos_train):
+        """Este metodo realiza una ordenacion de los individuos
+        en funcion del fitness para posteriormente realizar
+        una seleccion proporcional al mismo"""
+        poblacion = zip(self.individuos, map( lambda x : x.obtener_fitness(datos_train),
+                                                                           self.individuos))
+        poblacion_ordenada = sorted(poblacion,
+                                    key = lambda x : x[1],
+                                    reverse=True)
+        self.individuos = [individuo[0] for individuo in  poblacion_ordenada]
+        return poblacion_ordenada
+
     def __obtener_fitness_total( self, poblacion_ordenada):
         return sum(map(lambda x : x[1], poblacion_ordenada))
     def recombinacion (self,prob_cruce):
@@ -33,9 +56,13 @@ class Poblacion:
         fitness_total = self.__obtener_fitness_total(poblacion_ordenada)
 
         def __escoger_individuo():
-            for individuo in poblacion_ordenada:
-                if numero_aleatorio(0, fitness_total) < individuo[1]:
+            extremo_inferior = 0
+            valor = numero_aleatorio(0, fitness_total)
+            for individuo in self.individuos:
+                extremo_superior = extremo_inferior + individuo.fitness
+                if valor <= extremo_superior:
                     return individuo
+                extremo_inferior = extremo_superior
 
         for i in range(num_cruces):
             progenitor_1 = __escoger_individuo()
